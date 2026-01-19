0bb086f0db54408e04ec8e61ca1068a8e6ff25a2
lipi7almeida <felypefalmeida@hotmail.com>
2026-01-19 17:54:56 -0300

    update Historico 3.0

--- src/types/venda.ts
+++ src/types/venda.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-// src/types/venda.ts
-export type TipoVeiculo = "Carro" | "Moto";
-
-=======
->>>>>>> 60eb2ab61abd1ec5b3d1e9925bfdeaf43365d3e0
 export interface Servico {
   nome: string;
   preco: number;
@@ -18,10 +12,7 @@ export interface Venda {
   veiculo: string;
   modelo: string;
   placa: string;
-<<<<<<< HEAD
-=======
   telefone?: string;
->>>>>>> 60eb2ab61abd1ec5b3d1e9925bfdeaf43365d3e0
   servicos: Servico[];
   total: number;
   metodoPagamento: string;
